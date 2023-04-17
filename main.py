a0395c8bd06b1676c3da04ad85a61a9eadd8a300
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-04-17 09:39:57 +0200

    feat: Create repository structure

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon April 17 09:19:23 2023
+
+@author: Nicolle Mathieu
+"""
+import os
+import pandas as pd
+import requests
+from credentials import API_KEY
+
+
+if __name__ == '__main__':
+    # Main function
+    print("D4G")
