d88f645c7101a841698bdd68181807b5dbd72275
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-04-19 09:52:03 +0200

    refactor: Clean of some obselete function

--- main.py
+++ main.py
@@ -13,43 +13,7 @@ from credentials import API_KEY
 
 
 
-def determine_gps_coordinates_bomb_carbon(engine):
-    """Determine GPS Coordinates for each carbon bombs listed in Kuhne paper
-    """
-    file_path = "./data_sources/1-s2.0-S0301421522001756-mmc2.xlsx"
-    df = pd.read_excel(file_path, sheet_name='Full Carbon Bombs List',\
-                        engine='openpyxl', skipfooter = 4)
-    # Filter column
-    df = df[["Name","Country","Potential emissions (Gt CO2)","Fuel"]]
-    # Add columns lat and long with dummy values and partial match value
-    # Partial match value indicate whether location need to be manually check
-    # See https://developers.google.com/maps/documentation/javascript/geocoding?hl=fr
-    df["Latitude"] = 0.0
-    df["Longitude"] = 0.0
-    df["Partial_match"]=False
-    # Iteration over rows
-    for index, row in df.iterrows():
-        if engine == "Google":
-            address_call_api = "{0}, {1}".format(row["Name"],row["Country"])
-            lat,long,match = get_coordinates_google_api(address_call_api)
-        elif engine == "Wikipedia":
-            try:
-                title = row["Name"]
-                lat,long = get_coordinates_wikipedia_api(title)
-                match = True
-            except:
-                lat,long=np.nan,np.nan
-                match= False
-            
-            
-        else:
-            print("Engine non supported, Please provide a value in the followign list : 'Wikipedia','Google'")
-            sys.exit(0)
-        df.loc[index,"Latitude"] = lat
-        df.loc[index,"Longitude"] = long
-        df.loc[index,"Partial_match"] = match
-    # Save data to data_cleaned folder
-    df.to_csv("data_cleaned/bomb_carbon_gps.csv",index=False)
+
     
     
 def get_coordinates_google_api(address, api_key = API_KEY):
@@ -78,35 +42,6 @@ def get_coordinates_google_api(address, api_key = API_KEY):
         partial_match = True
     return latitude,longitude,partial_match
 
-def get_coordinates_wikipedia_api(title):
-    # Define the Wikipedia API URL
-    url = 'https://en.wikipedia.org/w/api.php'
-    # Define the parameters for the API request
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'prop': 'coordinates',
-        'titles': title,
-        'formatversion': 2,
-        'coprop': 'type|dim|globe|region|country'
-    }
-    # Make the API request
-    response = requests.get(url, params=params)
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
-        # Get the coordinates from the JSON data
-        try:
-            coordinates = data['query']['pages'][0]['coordinates'][0]
-            latitude = coordinates["lat"]
-            longitude = coordinates["long"]
-            return latitude, longitude
-        except KeyError:
-            print(f'Coordinates not found for the given title: {title}')
-            return None
-    else:
-        print(f'Error while making the API request. Status code: {response.status_code}')
-        return None
     
 def urgewald_database():
     file_path = "./data_sources/urgewald_GOGEL2022V1.xlsx"
