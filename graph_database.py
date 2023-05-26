f1443f6193171cb4a0c9fd6dde3f669e2ab2855f
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-05-26 14:08:07 +0200

    feat: Initiate Neo4j Graph Database script

---
+++ graph_database.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+To use this script, simply run it from the command line:
+$ python graph_database.py
+"""
+
+import os
+
+if __name__ == '__main__':
+    print("CarbonBombs")
