d1813c61b8c422a8c4708364741a0a48d4d53756
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-06-01 16:18:57 +0200

    refactor: Clean tutorial from graph_database

--- graph_database.py
+++ graph_database.py
@@ -9,49 +9,6 @@ import os
 import pandas as pd
 from neo4j import GraphDatabase, basic_auth
 
-def tutorial_movies_neo4j():
-    driver = GraphDatabase.driver(
-        "bolt://localhost:7687",
-        auth=basic_auth("neo4j", "password"))
-    imdb = pd.read_csv('./data_graphDB/imdb.csv')
-    # Cypher query to write nodes and relationships into DB
-    cypher_query = '''
-        MERGE (m:Movie {id: $id, title: $title, year: $year, rating: $rating, votes: $votes})
-        MERGE (d:Director {name: $director})
-        MERGE (d)-[:HAS_DIRECTED]->(m)
-        MERGE (a1:Actor {name: $star1})
-        MERGE (a2:Actor {name: $star2})
-        MERGE (a3:Actor {name: $star3})
-        MERGE (a4:Actor {name: $star4})
-        MERGE (a1)-[:HAS_ACTED_IN]->(m)
-        MERGE (a2)-[:HAS_ACTED_IN]->(m)
-        MERGE (a3)-[:HAS_ACTED_IN]->(m)
-        MERGE (a4)-[:HAS_ACTED_IN]->(m)
-    '''
-
-    # Function that runs the query by passing some properties
-    def create_movie(tx, id, title, year, rating, votes, director, star1, star2, star3, star4):
-        tx.run(cypher_query, id = id, title = title, year = year, rating = rating, votes = votes, director = director,
-            star1 = star1, star2 = star2, star3 = star3, star4 = star4)
-
-    # Open connection with DB, loop over the DataFrame's rows and write nodes/relationships into DB
-    with driver.session(database="neo4j") as session:
-        for i, row in imdb.iterrows():
-            id = i
-            title = row['Series_Title']
-            year = row['Released_Year']
-            rating = row['IMDB_Rating']
-            votes = row['No_of_Votes']
-            director = row['Director']
-            star1 = row['Star1']
-            star2 = row['Star2']
-            star3 = row['Star3']
-            star4 = row['Star4']
-            session.execute_write(create_movie, id, title, year, rating, votes, director, 
-                                    star1, star2, star3, star4)
-
-    driver.close()
-
 def load_renamed_columns():
     # create a mapping dictionary for carbon bombs columns
     carbon_bombs_new_column = {
@@ -104,51 +61,8 @@ def purge_current_database():
     with driver.session() as session:
         session.execute_write(delete_all)
     driver.close()
-
-def tutorial_got_neo4j():
-    # Read list of characters (nodes)
-    nodes = pd.read_csv("./data_graphDB/asoiaf-all-nodes.csv")
-    # Read list of interactions (relationships)
-    edges = pd.read_csv("./data_graphDB/asoiaf-all-edges.csv")
-    # Connect to the database
-    driver = GraphDatabase.driver(
-        "bolt://localhost:7687",
-        auth=basic_auth("neo4j", "password"))
-    # Write nodes into DB
-    cypher_query = '''
-        MERGE (n:Character {id: $id, name: $name})
-        RETURN n.id, n.name as person
-    '''
-
-    def create_character(tx, id, name):
-        tx.run(cypher_query, id = id, name = name)
-
-    with driver.session(database="neo4j") as session:
-        for i, row in nodes.iterrows():
-            id = row['Id']
-            name = row['Label']
-            session.execute_write(create_character, id, name)
-
-    driver.close()
-    # Write relationships into DB
-    cypher_query = '''
-        MATCH (c1:Character {id: $id1})
-        MATCH (c2:Character {id: $id2})
-        MERGE (c1)-[:INTERACTS_WITH {weight: $weight}]->(c2)
-    '''
-
-    def create_interaction(tx, id1, id2, weight):
-        tx.run(cypher_query, id1 = id1, id2 = id2, weight = weight)
-
-    with driver.session(database="neo4j") as session:
-        for i, row in edges.iterrows():
-            id1 = row['Source']
-            id2 = row['Target']
-            weight = row['weight']
-            session.execute_write(create_interaction, id1, id2, weight)
-
-    driver.close()
-
+    
+    
 def carbon_bombs_graph_database():
     # Define driver to connect to neo4j Database
     driver = GraphDatabase.driver(
@@ -215,7 +129,6 @@ def carbon_bombs_graph_database():
     # Close connection to database
     driver.close()    
     
-
 def write_nodes(driver,node_type,node_data):
     def create_nodes(tx, node_type, node_data):
         cypher_query_carbon_bombs = f"MERGE (n:{node_type} " + "{"
@@ -232,14 +145,10 @@ def write_nodes(driver,node_type,node_data):
 
 
 if __name__ == '__main__':
-
     purge_current_database()
-    #tutorial_movies_neo4j()
-    #tutorial_got_neo4j()
     carbon_bombs_graph_database()
-
-
     """
+    Cypher Query examples
     MATCH (n:Carbon_bomb) RETURN n LIMIT 25
-    MATCH (n:Character) RETURN n LIMIT 25
+    MATCH (n:Company) RETURN n LIMIT 25
     """
