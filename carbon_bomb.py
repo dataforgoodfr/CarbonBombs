e78876027ab9ef7b5d442ee0139a5a730661fa73
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-08-31 17:26:19 +0200

    feat: Add protection on adding GPT data if empty

--- carbon_bomb.py
+++ carbon_bomb.py
@@ -1166,21 +1166,22 @@ def complete_GEM_with_ChatGPT(df):
                   left_on=['Carbon_bomb_name_source_CB', "Country_source_CB"],
                   right_on=['Name', "Country"],
                   suffixes=('', ''))
-    df['Carbon_bomb_description_source'] = df.apply(lambda row: 'ChatGPT' if \
-                                             (row['Carbon_bomb_description']=='No description available'\
-                                              or row['Carbon_bomb_description']=='New project') \
-                                              else 'GEM', axis=1)
+    df['Carbon_bomb_description_source'] = df.apply(lambda row: 'ChatGPT' if 
+                ((row['Carbon_bomb_description']=='No description available'\
+                or row['Carbon_bomb_description']=='New project') \
+                and not pd.isnull(row['Description'])) else 'GEM', axis=1)
     df['Carbon_bomb_start_year_source'] = df.apply(lambda row: 'ChatGPT' if \
-                                             row['Carbon_bomb_start_year'] == 'No start year available'\
-                                             else 'GEM', axis=1)
+                (row['Carbon_bomb_start_year'] == 'No start year available'\
+                and not pd.isnull(row['Start year'])) else 'GEM', axis=1)
     df['Carbon_bomb_start_year'] = df.apply(lambda row: row['Start year'] if \
-                                            row['Carbon_bomb_start_year'] == 'No start year available' \
-                                            else row['Carbon_bomb_start_year'], axis=1)
+                (row['Carbon_bomb_start_year'] == 'No start year available' \
+                and not pd.isnull(row['Start year'])) \
+                else row['Carbon_bomb_start_year'], axis=1)
     df['Carbon_bomb_description'] = df.apply(lambda row: row['Description'] if \
-                                             (row['Carbon_bomb_description']=='No description available'\
-                                              or \
-                                              row['Carbon_bomb_description']=='New project') \
-                                             else row['Carbon_bomb_description'], axis=1)
+                ((row['Carbon_bomb_description']=='No description available'\
+                or row['Carbon_bomb_description']=='New project') \
+                and not pd.isnull(row['Description'])) \
+                else row['Carbon_bomb_description'], axis=1)
                                                     
     df=df.drop(labels=['Name', 'Start year', 'Description','Country'], axis=1)
                                                     
