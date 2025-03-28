3be42058347f0b09075537767a2341dae893da89
Mathieu Nicolle <mnicolle.utc@gmail.com>
2025-03-28 16:53:32 +0100

    ✨ Create structure for LNG table

---
+++ carbon_bombs/processing/lng.py
@@ -0,0 +1,12 @@
+"""Function to process lng informations from GOGEL"""
+import pandas as pd
+from carbon_bombs.utils.logger import LOGGER
+
+
+def create_lng_table():
+    """
+    Mock documentation
+    """
+    df = pd.DataFrame(columns=["mock"])
+    LOGGER.info("test")
+    return df
