ea307819286fc6e77194cb1c61837b4284642ceb
Mathieu Nicolle <mnicolle.utc@gmail.com>
2025-05-25 19:52:50 +0200

    ✨ Update comparison v1 v2 with carbon bombs > 0.1 GtCO2

--- carbon_bombs/conf.py
+++ carbon_bombs/conf.py
@@ -39,7 +39,8 @@ FPATH_SRC_UNDATA_CO2 = (
     f"{DATA_SOURCE_PATH}/undata_SYB65_310_202209_Carbon Dioxide Emission Estimates.csv"
 )
 FPATH_SRC_RYSTAD_CB = f"{DATA_SOURCE_PATH}/Carbon_Bombs_Projects.xlsx"
-SHEETNAME_RYSTAD_CB_EMISSION = "<1GT (no data publication)"
+SHEETNAME_RYSTAD_CB_EMISSION_INFERIOR_1GT = "V1_method_0.1GT"
+SHEETNAME_RYSTAD_CB_EMISSION = "Carbon_Bombs_1GT"
 SHEETNAME_RYSTAD_CB_COMPANY = "Carbon_Bombs_1GT_Companies"
 SHEETNAME_RYSTAD_GASOIL_EMISSION = "Carbon_Bombs_Projects"
 
