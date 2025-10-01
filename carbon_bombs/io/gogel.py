daa54fdb94daa74b9f111c9cd8a45fa750c09ecf
Mathieu Nicolle <mnicolle.utc@gmail.com>
2025-10-01 21:21:47 +0200

    🚧 WIP 11

--- carbon_bombs/io/gogel.py
+++ carbon_bombs/io/gogel.py
@@ -16,18 +16,22 @@ def load_lng_database():
     """
     LOGGER.debug("Read GOGEL data: all LNG project")
     df = pd.read_excel(
-        FPATH_SRC_GOGEL_LNG,
-        sheet_name="LNG Liquefaction projects",
-        engine="openpyxl",
-        skiprows=2,
+        FPATH_SRC_GOGEL_LNG, sheet_name="LNG Liquefaction projects", engine="openpyxl"
     )
+    parent_cols = [f"Parent {i}" for i in range(1, 11)]
+
+    df["Companies involved"] = df[parent_cols].apply(
+        lambda row: ",".join(row.dropna().astype(str)), axis=1
+    )
+
     renamed_columns = {
-        "Name (project)": "project_name",
-        "Other Name": "other_name",
-        "Unit": "unit",
+        "Project Name": "project_name",
         "Export capacity (Mtpa)": "export_capacity_in_mtpa",
         "Status": "project_status",
+        "Start of commercial operations": "start_year",
         "Country": "country",
+        "latitude": "latitude",
+        "longitude": "longitude",
         "Companies involved": "companies_involved",
     }
     # Only keep columns of interest for the project
@@ -35,16 +39,21 @@ def load_lng_database():
     # Rename columns
     df = df.rename(columns=renamed_columns)
 
-    # For duplicate name in project_name column we use other_name values
-    mask = df["project_name"].duplicated(keep=False) & df["other_name"].notna()
-    df.loc[mask, "project_name"] = df.loc[mask, "other_name"]
+    # Deprecated
+    # # For duplicate name in project_name column we use other_name values
+    # mask = df["project_name"].duplicated(keep=False) & df["other_name"].notna()
+    # df.loc[mask, "project_name"] = df.loc[mask, "other_name"]
 
-    # If other_name is empty (NaN or ""), concatenate project_name and unit name
-    mask = df["project_name"].duplicated(keep=False)
-    df.loc[mask, "project_name"] = df["project_name"] + " " + df["unit"]
+    # # If other_name is empty (NaN or ""), concatenate project_name and unit name
+    # mask = df["project_name"].duplicated(keep=False)
+    # df.loc[mask, "project_name"] = df["project_name"] + " " + df["unit"]
 
-    # Drop column other_name
-    df = df.drop(columns=["other_name", "unit"])
+    # # Drop column other_name
+    # df = df.drop(columns=["other_name", "unit"])
     # Replace UAE by United Arab Emirates in country column
     df["country"] = df["country"].replace("UAE", "United Arab Emirates")
+    # Replace country Senegal/Mauritania for project Greater Tortue Ahmeyim - Phase 1 and Phase 2
+    # To avoid warning on double country
+    # See slack message : https://data-for-good.slack.com/archives/C08C639D8HM/p1755531343983969?thread_ts=1754847139.766209&cid=C08C639D8HM
+    df["country"] = df["country"].replace("Senegal/Mauritania", "Senegal")
     return df
--- carbon_bombs/processing/lng.py
+++ carbon_bombs/processing/lng.py
@@ -25,30 +25,6 @@ def create_lng_table():
     """
     LOGGER.debug("Read LNG source: LNG Liquefaction projects")
     df_lng = load_lng_database()
-
-    # Replace country Senegal/Mauritania for project Greater Tortue Ahmeyim - Phase 1 and Phase 2
-    # To avoid warning on double country
-    # See slack message : https://data-for-good.slack.com/archives/C08C639D8HM/p1755531343983969?thread_ts=1754847139.766209&cid=C08C639D8HM
-    df_lng["country"] = df_lng["country"].replace("Senegal/Mauritania", "Senegal")
-
-    # TODO: refacto get lat long country in an utils func for here and cb processing
-    country_lat_long_df = pd.read_csv(f"{DATA_SOURCE_PATH}/longitude-latitude.csv")
-
-    LOGGER.debug("Add LNG project's country location")
-    df_lng[["latitude", "longitude"]] = df_lng["country"].apply(
-        lambda x: pd.Series(_get_lat_long(x, country_lat_long_df))
-    )
-
-    # Add noise to duplicate lat/long
-    np.random.seed(42)
-    lat_long_dup = df_lng.duplicated(subset=["latitude", "longitude"], keep=False)
-    df_lng.loc[lat_long_dup, "latitude"] = df_lng.loc[lat_long_dup, "latitude"].apply(
-        _add_noise_lat_long
-    )
-    df_lng.loc[lat_long_dup, "longitude"] = df_lng.loc[lat_long_dup, "longitude"].apply(
-        _add_noise_lat_long
-    )
-    LOGGER.debug("Success adding LNG project's country location")
     return df_lng
 
 
